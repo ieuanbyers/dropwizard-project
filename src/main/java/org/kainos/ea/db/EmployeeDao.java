c85ad0b13c6ebc6a00f2b69c13bf05f7c4f554f4
Ieuan Byers <ieuan.byers@kainos.com>
2023-06-29 11:11:47 +0100

    Adding employee dao with insert method

---
+++ src/main/java/org/kainos/ea/db/EmployeeDao.java
@@ -0,0 +1,33 @@
+package org.kainos.ea.db;
+
+import org.kainos.ea.cli.Employee;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class EmployeeDao {
+    public int createDeliveryEmployee(Employee employee) throws SQLException{
+        Connection c = DatabaseConnector.getConnection();
+
+        String insertStatement = "INSERT INTO DeliveryEmployee(Name, Salary, BankAccountNo, NatInsuranceNo) VALUES (?, ?, ?, ?);";
+
+        PreparedStatement st = c.prepareStatement(insertStatement);
+
+        st.setString(1, employee.getName());
+        st.setDouble(2, employee.getSalary());
+        st.setString(3, employee.getBankAccountNo());
+        st.setString(4, employee.getNatInsuranceNo());
+
+        st.executeUpdate();
+
+        ResultSet rs = st.getGeneratedKeys();
+
+        if (rs.next()){
+            return rs.getInt(1);
+        }
+
+        return -1;
+    }
+}
