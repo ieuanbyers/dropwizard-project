fe4ebca324a3b872494e141f9e66e9276f037c36
leeservice <lee.service@kainos.com>
2023-06-29 13:24:07 +0100

    Adding the getEmployeeByID in EmployeeService / EmployeeDao and the new FailedToGetEmployeeException

--- src/main/java/org/kainos/ea/db/EmployeeDao.java
+++ src/main/java/org/kainos/ea/db/EmployeeDao.java
@@ -1,13 +1,15 @@
 package org.kainos.ea.db;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import org.kainos.ea.cli.Employee;
+import org.kainos.ea.cli.EmployeeRequest;
+
+import java.sql.*;
+
+import static org.kainos.ea.db.DatabaseConnector.getConnection;
 
 public class EmployeeDao {
     public int createDeliveryEmployee(EmployeeRequest employee) throws SQLException{
-        Connection c = DatabaseConnector.getConnection();
+        Connection c = getConnection();
 
         String insertStatement = "INSERT INTO DeliveryEmployee(Name, Salary, BankAccountNo, NatInsuranceNo) VALUES (?, ?, ?, ?);";
 
@@ -28,4 +30,29 @@ public class EmployeeDao {
 
         return -1;
     }
+
+    public Employee getEmployeeByID(int id) throws SQLException {
+        Connection c = getConnection();
+        Statement st = c.createStatement();
+
+        ResultSet rs = st.executeQuery(
+                "SELECT (`Name`,Salary,BankAccountNo,NatInsuranceNo) FROM `DeliveryEmployee`"
+                + "WHERE DeliveryEmployeeID = "+id);
+
+
+        while (rs.next()) {
+            return new Employee(
+                    rs.getInt("DeliveryEmployeeID"),
+                    rs.getString("Name"),
+                    rs.getFloat("Salary"),
+                    rs.getString("BankAccountNo"),
+                    rs.getString("NatInsuranceNo")
+            );
+
+        }
+        return null;
+    }
+
+
+
 }
